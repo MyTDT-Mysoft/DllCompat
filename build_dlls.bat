7dc0cb565846a17ae9426df306ff90e9882d0e77
pachuco <Ifail@life.net>
2017-08-25 16:48:37 +0300

    Move files around for test compile script.

---
+++ build_dlls.bat
@@ -0,0 +1,46 @@
+@echo off
+setlocal
+
+set params=advapi3x credux gdi3x kernel3x msvcrx opengl3x powrprox shell3x user3x vcruntime140 ws2_3x
+set settfile=compile.ini
+set binpath=bin\dll
+set srcpath=src\dll
+
+for /f "delims== tokens=1,2" %%G in (%settfile%) do set %%G=%%H
+IF NOT EXIST %binpath% mkdir %binpath%
+del %binpath%\* /Q
+call :comploop %params%
+goto :eof
+
+:comploop
+call :compile %1
+shift /1
+if not "%~1" EQU "" goto :comploop
+
+:donecomp
+echo Copying DLLs to windir
+copy %binpath%\*x.dll %windir% /Y
+:cleanup
+echo Cleaning compile residue
+del %binpath%\*.dll.a /Q
+del %binpath%\*.o /Q
+del %binpath%\*.s /Q
+endlocal
+pause
+goto :eof
+
+:failed
+echo Compilation Failed...
+rem pause >nul
+goto :cleanup
+
+:compile
+echo compiling %1
+pushd .\%srcpath%\%1
+%dllc_fbcpath% -dll %1.bas -Wl "%1.dll.def" -x ..\..\..\%binpath%\%1.dll -i ..\..\..\src
+set err=%errorlevel%
+popd
+if %err% gtr 0 goto :Failed
+goto :eof
+
+:eof
