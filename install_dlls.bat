e68752a616a56c10be8b463ca3a3a9be478d707f
pachuco <Ifail@life.net>
2018-09-30 20:41:25 +0300

    Scratch that, custom dlls in system32 cannot be used with dynamic load for some reason. Move hardlinks to %windir%

--- install_dlls.bat
+++ install_dlls.bat
@@ -4,6 +4,6 @@ echo Copying DLLs to windir
 copy bin\dll\*.dll %windir%\appcompat /Y
 copy bin\guest_dll\*.dll %windir%\appcompat /Y
 
-for %%I in (%windir%\appcompat\*.dll) do bin\util\makelink.exe /H %windir%\system32\%%~nxI %windir%\appcompat\%%~nxI
+for %%I in (%windir%\appcompat\*.dll) do bin\util\makelink.exe /H %windir%\%%~nxI %windir%\appcompat\%%~nxI
 
 pause
