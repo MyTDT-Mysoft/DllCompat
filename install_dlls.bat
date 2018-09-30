ed923204dc6dc732d59bf804d11cf82045d9ef39
pachuco <Ifail@life.net>
2018-09-30 21:11:29 +0300

    Mysoft says we don't need hardlinking no more.

--- install_dlls.bat
+++ install_dlls.bat
@@ -1,9 +1,9 @@
 @echo off
 
 echo Copying DLLs to windir
-copy bin\dll\*.dll %windir%\appcompat /Y
-copy bin\guest_dll\*.dll %windir%\appcompat /Y
+copy bin\dll\*.dll %windir% /Y
+copy bin\guest_dll\*.dll %windir% /Y
 
-for %%I in (%windir%\appcompat\*.dll) do bin\util\makelink.exe /H %windir%\%%~nxI %windir%\appcompat\%%~nxI
+::for %%I in (%windir%\appcompat\*.dll) do bin\util\makelink.exe /H %windir%\%%~nxI %windir%\appcompat\%%~nxI
 
 pause
