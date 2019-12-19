434c07a980a02cd6b1396bf39767755563b319a8
pachuco <ifail@life.net>
2019-12-19 16:57:12 +0200

    Added new guest dll, propsys.

--- install_dlls.bat
+++ install_dlls.bat
@@ -3,11 +3,13 @@
 set settfile=compile.ini
 for /f "delims== tokens=1,2" %%G in (%settfile%) do set %%G=%%H
 
-echo Copying DLLs to %install_path%
-copy bin\dll\*.dll %install_path% /Y
-copy bin\guest_dll\*.dll %install_path% /Y
+echo Copying DLLs to %install_path% and installing COM servers
+xcopy bin\dll\*.dll %install_path% /Y
+xcopy bin\guest_dll\*.dll /exclude:bin\guest_dll\special.txt %install_path% /Y
 
-echo Installing COM servers
 regsvr32 /s %install_path%\comdlg3x.dll
-
+if not exist %WINDIR%\system32\propsys.dll (
+  xcopy bin\guest_dll\propsys.dll %install_path% /Y
+  regsvr32 /s %install_path%\propsys.dll
+)
 pause
