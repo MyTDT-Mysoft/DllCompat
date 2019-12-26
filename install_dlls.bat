04a2156a365fb295c39325f6e530afc38ee181b6
pachuco <ifail@life.net>
2019-12-26 19:16:42 +0200

    Whoops! install_dlls.bat was failing on first copy failure and abandoning rest of files.

--- install_dlls.bat
+++ install_dlls.bat
@@ -4,12 +4,12 @@ set settfile=compile.ini
 for /f "delims== tokens=1,2" %%G in (%settfile%) do set %%G=%%H
 
 echo Copying DLLs to %install_path% and installing COM servers
-xcopy bin\dll\*.dll %install_path% /Y
-xcopy bin\guest_dll\*.dll /exclude:bin\guest_dll\special.txt %install_path% /Y
+xcopy bin\dll\*.dll %install_path% /c /Y
+xcopy bin\guest_dll\*.dll /exclude:bin\guest_dll\special.txt %install_path% /c /Y
 
 regsvr32 /s %install_path%\comdlg3x.dll
 if not exist %WINDIR%\system32\propsys.dll (
-  xcopy bin\guest_dll\propsys.dll %install_path% /Y
+  xcopy bin\guest_dll\propsys.dll %install_path% /c /Y
   regsvr32 /s %install_path%\propsys.dll
 )
 pause
